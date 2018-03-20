1a3d2cabcf6ed6006d0c6b057bcee12ebba681bc
Thomas Foucault <t.foucault2@gmail.com>
2018-03-20 17:14:56 +0100

    Smooth character rotation

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -4,70 +4,70 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-  [SerializeField]
-  private List<Transform> playersToFollow;
+    [SerializeField]
+    private List<Transform> playersToFollow;
 
-  private float minHeight = 15f;
+    private float minHeight = 15f;
 
-  private void FixedUpdate() {
-    transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0, 0);
-    FixedCameraFollowSmooth(GetComponent<Camera>(), playersToFollow);
-  }
+    private void FixedUpdate() {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 0, 0);
+        FixedCameraFollowSmooth(GetComponent<Camera>(), playersToFollow);
+    }
 
-  // Follow two transform
-  public void FixedCameraFollowSmooth(Camera cam, List<Transform> tfs) {
-    float zoomFactor = 1.4f;
-    float followTimeDelta = 0.8f;
+    // Follow two transform
+    public void FixedCameraFollowSmooth(Camera cam, List<Transform> tfs) {
+        float zoomFactor = 1.4f;
+        float followTimeDelta = 0.8f;
 
-    Vector3 midpoint;
-    float distance;
+        Vector3 midpoint;
+        float distance;
 
-    //Search for the farthest transforms from each other
-    if (tfs.Count > 2) {
-      Transform t1 = tfs[0];
-      Transform t2 = tfs[1];
-      float maxDistance = -1f;
+        //Search for the farthest transforms from each other
+        if (tfs.Count > 2) {
+            Transform t1 = tfs[0];
+            Transform t2 = tfs[1];
+            float maxDistance = -1f;
 
-      for (int i = 0; i < tfs.Count - 1; i++) {
-        for (int j = i + 1; j < tfs.Count; j++) {
-          distance = (tfs[i].position - tfs[j].position).magnitude;
-          if (maxDistance < distance) {
-            maxDistance = distance;
-            t1 = tfs[i];
-            t2 = tfs[j];
-          }
+            for (int i = 0; i < tfs.Count - 1; i++) {
+                for (int j = i + 1; j < tfs.Count; j++) {
+                    distance = (tfs[i].position - tfs[j].position).magnitude;
+                    if (maxDistance < distance) {
+                        maxDistance = distance;
+                        t1 = tfs[i];
+                        t2 = tfs[j];
+                    }
+                }
+            }
+
+            Vector3 averagePos = Vector3.zero;
+            foreach (Transform t in tfs) {
+                averagePos += t.position;
+            }
+
+            midpoint = averagePos / tfs.Count;
+            distance = maxDistance;
+        } else {
+            midpoint = (tfs[0].position + tfs[1].position) / 2f;
+            distance = (tfs[0].position - tfs[1].position).magnitude;
         }
-      }
 
-      Vector3 averagePos = Vector3.zero;
-      foreach (Transform t in tfs) {
-        averagePos += t.position;
-      }
+        // Move camera a certain distance
+        Vector3 cameraDestination = midpoint - cam.transform.forward * distance * zoomFactor;
 
-      midpoint = averagePos / tfs.Count;
-      distance = maxDistance;
-    } else {
-      midpoint = (tfs[0].position + tfs[1].position) / 2f;
-      distance = (tfs[0].position - tfs[1].position).magnitude;
+        // Avoid zooming too close
+        if (cameraDestination.y < minHeight) {
+            cameraDestination.y = minHeight;
+        }
+
+        // Move camera lower to balance the x rotation
+        cameraDestination.z -= 2;
+
+        // You specified to use MoveTowards instead of Slerp
+        cam.transform.position = Vector3.Slerp(cam.transform.position, cameraDestination, followTimeDelta);
+
+        // Snap when close enough to prevent annoying slerp behavior
+        if ((cameraDestination - cam.transform.position).magnitude <= 0.05f)
+            cam.transform.position = cameraDestination;
     }
-    
-    // Move camera a certain distance
-    Vector3 cameraDestination = midpoint - cam.transform.forward * distance * zoomFactor;
-    
-    // Avoid zooming too close
-    if(cameraDestination.y < minHeight) {
-      cameraDestination.y = minHeight;
-    }
-
-    // Move camera lower to balance the x rotation
-    cameraDestination.z -= 2;
-    
-    // You specified to use MoveTowards instead of Slerp
-    cam.transform.position = Vector3.Slerp(cam.transform.position, cameraDestination, followTimeDelta);
-
-    // Snap when close enough to prevent annoying slerp behavior
-    if ((cameraDestination - cam.transform.position).magnitude <= 0.05f)
-      cam.transform.position = cameraDestination;
-  }
 
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,9 +1,9 @@
 ﻿using UnityEngine;
 
-namespace Assets.Scripts.Player
-{
-    public class PlayerController : MonoBehaviour
-    {
+namespace Assets.Scripts.Player {
+    public class PlayerController : MonoBehaviour {
+        private const float ROTATION_SPEED = 20f;
+
         public float Speed;
         public float DashSpeed;
         public int DashTime;
@@ -12,15 +12,14 @@ namespace Assets.Scripts.Player
         private int _currentDashTime;
         private float _defaultPositionY;
 
-        private void Start()
-        {
+
+        private void Start() {
             _characterController = GetComponent<CharacterController>();
             _currentDashTime = DashTime;
             _defaultPositionY = transform.position.y + _characterController.skinWidth;
         }
 
-        private void FixedUpdate()
-        {
+        private void FixedUpdate() {
             var moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             var speed = Speed;
 
@@ -30,22 +29,24 @@ namespace Assets.Scripts.Player
                     _defaultPositionY,
                     transform.position.z);
 
-            if (moveDirection.sqrMagnitude > 0.1f)
-                transform.rotation = Quaternion.LookRotation(moveDirection);
+            if (moveDirection.sqrMagnitude > 0.05f)
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection), Time.deltaTime * ROTATION_SPEED);
 
-            if (Input.GetButtonDown("Dash") && _currentDashTime == DashTime)
-            {
+            if (Input.GetButtonDown("Dash") && _currentDashTime == DashTime) {
                 _currentDashTime = 0;
                 Debug.Log("Dash");
             }
 
-            if (_currentDashTime < DashTime)
-            {
+            if (_currentDashTime < DashTime) {
                 speed = DashSpeed;
                 _currentDashTime++;
             }
 
             _characterController.Move(moveDirection * Time.deltaTime * speed);
+
+        }
+
+        private void Rotate(Vector3 destination) {
 
         }
     }
